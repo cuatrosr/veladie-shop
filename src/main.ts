3f7b7acf95ba52fb0d9b31874e09e13d55a90d5a
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-11-29 15:01:07 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(
